e3ca9772969c78b1e21eadbcd0e73e034fa911ff
hanifk2017 <hanifk@yahoo.com>
2018-05-31 22:14:41 +0100

    Added in H2 load script to provide data as in spec

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO account (ID,
+					FIRST_NAME, 
+					SECOND_NAME,
+					ACCOUNT_NUMBER
+					) 
+	VALUES
+  					(1, 'John', 'Doe', '1234');
+ INSERT INTO account (ID,
+					FIRST_NAME, 
+					SECOND_NAME,
+					ACCOUNT_NUMBER
+					) 
+	VALUES
+  					(2, 'Jim', 'Doe', '1235');
+INSERT INTO account (ID,
+					FIRST_NAME, 
+					SECOND_NAME,
+					ACCOUNT_NUMBER
+					) 
+	VALUES
+  					(3, 'John', 'Taylor', '1236');
